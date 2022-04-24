51ccf6faa8b67ab8983029c6583030ad59080c86
Ozan Bayram <okutbay@gmail.com>
2022-04-25 01:07:59 +0300

    Added extensions sample to web app

---
+++ FabrikafaSample/Pages/Extensions.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FabrikafaSample.Pages
+{
+    public class ExtensionsModel : PageModel
+    {
+        private readonly ILogger<ExtensionsModel> _logger;
+
+        public ExtensionsModel(ILogger<ExtensionsModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
