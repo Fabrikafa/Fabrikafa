b024afe6b73d690482622854e29caf29e128ac8a
Ozan K. Bayram <okutbay@gmail.com>
2026-01-17 22:15:34 +0300

    Improve Result class.

--- Fabrikafa/Result.cs
+++ Fabrikafa/Result.cs
@@ -1,12 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace Fabrikafa;
 
 [Serializable]
 public class Result
 {
+    internal static class MessageText
+    {
+        internal const string GeneralErrorText = "General error occured";
+        internal const string GoToHomeText = "Go to Home";
+    }
+
     /// <summary>
     /// Bolean result of the operation.
     /// </summary>
@@ -82,13 +86,13 @@ public class Result
         this.Code = ResultCodeEnum.GeneralError;
         this.MessageType = MessageTypeEnum.error;
         this.Title = MessageType.ToString();
-        messageText = "General error occured";
+        messageText = $"{MessageText.GeneralErrorText}.";
         codeText = ((int)this.Code).ToString();
         this.FriendlyDescription = template;
         CallToActionUrl = string.Empty;
         CallToActionUrlText = string.Empty;
         CallBackUrl = "/";
-        CallBackUrlText = "Go to Home";
+        CallBackUrlText = $"{MessageText.GoToHomeText}";
     }
 
     /// <summary>
@@ -106,7 +110,7 @@ public class Result
 
         if (!Enum.IsDefined(typeof(ResultCodeEnum), enumValue) && !enumValue.ToString().Contains(","))
         {
-            messageText = $"General error occured: {Message}";
+            messageText = $"{MessageText.GeneralErrorText}: {Message}";
         }
         else
         {
